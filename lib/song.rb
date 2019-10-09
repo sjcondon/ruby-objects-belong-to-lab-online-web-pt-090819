8f1b54aa9504280302f323c7e937da26adeef883
sarah <s.j.condon01@gmail.com>
2019-10-09 20:26:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,2 @@
 class Song
-  attr_accessor :name, :artist
-@@all = []
-  
-  def initialize
-    # @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-  
-  def artist_name
-    if !self.artist
-      return nil
-    else
-      self.artist.name
-    end
-  end
-end
+  attr_accessor :title, :artist
