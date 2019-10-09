6d716bc42c0b32b77ca6596c6efd46fc0b1912fb
sarah <s.j.condon01@gmail.com>
2019-10-09 20:28:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,33 +1,33 @@
-# class Author
+class Author
 
-#   attr_accessor :name 
+  attr_accessor :name 
 
-#   @@all = []
+  @@all = []
   
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-#   def self.all 
-#     @@all
-#   end
+  def self.all 
+    @@all
+  end
 
-#   def post
-#     Post.all.select{|post| post.author == self }
-#   end
+  def post
+    Post.all.select{|post| post.author == self }
+  end
   
-#   def add_post(post)
-#   post.author = self
-#   end
+  def add_post(post)
+  post.author = self
+  end
 
-#   def add_post_by_title(title)
-#     new_post = Post.new(title)
-#     add_post(new_post)
-#   end
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    add_post(new_post)
+  end
 
-#   def self.post_count
-#   Post.all.count 
-#   end
+  def self.post_count
+  Post.all.count 
+  end
 
-# end
+end
